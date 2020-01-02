53676f874237c3de7c7b319534d9327d2a9b8e23
Chris Harding <chrish@cantab.net>
2020-01-02 12:22:41 +0000

    Add before wipe

---
+++ ActionCamera/ActionCameraSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ActionCameraSample
+//
+//  Created by Chris Harding on 11/20/13.
+//  Copyright (c) 2013 Motrr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ActionCamera/ActionCameraSampleTests/ActionCameraSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  ActionCameraSampleTests.m
+//  ActionCameraSampleTests
+//
+//  Created by Chris Harding on 11/20/13.
+//  Copyright (c) 2013 Motrr. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ActionCameraSampleTests : XCTestCase
+
+@end
+
+@implementation ActionCameraSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
